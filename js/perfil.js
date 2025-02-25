756629f1733218039e0ec9c358471730db8ae6b2
Mihai Gidu <mihaitagidudaniel.com>
2025-02-25 13:25:40 +0100

    1

--- js/perfil.js
+++ js/perfil.js
@@ -34,10 +34,10 @@ async function cargarPerfilUsuario() {
 
             // Actualizar la interfaz con la información del usuario
             document.querySelector(".profile-pic").src = usuario.profilePicture || "imagenes/FotonUsuario.jpg";
-            document.querySelector("h2").textContent = usuario.name || "Nombre Desconocido";
+            document.querySelector("h2").textContent = usuario.name || "Error A2";
             document.querySelector(".text-muted").textContent = `Usuario desde ${usuario.createdAt?.substring(0, 4) || "N/A"}`;
             document.querySelector("p strong:nth-of-type(1)").nextSibling.textContent = ` ${usuario.email}`;
-            document.querySelector("p strong:nth-of-type(2)").nextSibling.textContent = ` ${usuario.suscripcion || "Gratis"}`;
+            document.querySelector("p strong:nth-of-type(2)").nextSibling.textContent = ` ${usuario.rol || "error"}`;
 
         } else {
             console.error("No se pudo cargar la información del usuario.");
